3f4171a26b07bdacd239cbd40c914a4b10546da1
Kun Liu <kunliu.work@gmail.com>
2016-12-27 19:14:27 +1100

    OPP concepts

---
+++ Object oriented programming/1. Declare JavaScript Objects as Variables.js
@@ -0,0 +1,20 @@
+// Declare JavaScript Objects as Variables
+// Before we dive into Object Oriented Programming, let's revisit JavaScript objects.
+
+// Give your motorBike object a wheels, engines and seats attribute and set them to numbers.
+
+
+var car = {
+  "wheels":4,
+  "engines":1,
+  "seats":5
+};
+
+var motorBike = {
+  "wheels":3,
+  "engines":3,
+  "seats":5
+
+  // Only change code below this line.
+
+};
