2cdb037ef737c5bed2206d1e525179d75d67d9e3
Kun Liu <kunliu.work@gmail.com>
2016-12-27 19:11:50 +1100

    Basic Algorithm Scripting

---
+++ Algorithym/2. Factorialize a Number.js
@@ -0,0 +1,35 @@
+/*Factorialize a Number
+Return the factorial of the provided integer.
+
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+Arithmetic Operators*/
+
+
+function factorialize(num) {
+/*  
+method 1
+  var result = 1;
+  for(var i = num;i>0;i--){
+    result = result * i;
+  }
+  num = result;
+  return num;
+}
+*/
+  //method 2
+  var result = 1;
+  for (num;num>0;num--){
+    result *=num;
+  }
+  return result;
+}
+factorialize(5);
