4fa9507e0068d3068d3f9c54434fd20d41be837c
Tony Liu <tonykunliu@Tonys-MacBook-Pro.local>
2017-01-30 11:41:13 +1100

    add one Algorithym exercise solution

---
+++ Algorithym/7. Confirming the end words.js
@@ -0,0 +1,36 @@
+/*
+Confirm the Ending 
+Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+String.prototype.substr()
+String.prototype.substring()
+*/
+
+
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+  var i, j;
+  i = str.length;
+  j = target.length * (-1);
+  var string;
+  string = str.substr(j);
+  console.log(i);
+  if (string === target){
+    
+    return true;
+  }
+  else 
+    return false;
+  
+  
+  
+}
+
+confirmEnding("Bastian", "n");
